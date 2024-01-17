0774279a6e1410d69c39cf983d81f6f7ea3e7f74
Николай <gulyaev132415@yandex.ru>
2024-01-17 22:53:03 +0500

    3 work

--- work3/src/main/java/org/example/repository/T1repository.java
+++
@@ -1,8 +0,0 @@
-package org.example.repository;
-
-
-import org.example.model.City;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface T1repository extends JpaRepository<City, Long> {
-}
--- work3/src/main/java/org/example/service/T1service.java
+++
@@ -1,4 +0,0 @@
-package org.example.service;
-
-public interface T1service  {
-}
